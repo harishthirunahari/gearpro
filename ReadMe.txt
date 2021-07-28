6df4773ecb15bcedebea3af50c5bc1bc3aacf137
Harish Thirunahari <harish.thirunahari@hexagon.com>
2021-07-28 10:11:40 +0530

    active link changes

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+ng build --prod --base-href "https://harishthirunahari.github.io/gearpro/"
+ngh --dir dist/gearpro-app
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gearpro-app';
+  @ViewChild("elemHome") elemHome: ElementRef;
+  @ViewChild("elemProducts") elemProducts: ElementRef;
+  @ViewChild("elemServices") elemServices: ElementRef;
+  @ViewChild("elemContactUs") elemContactUs: ElementRef;
+
+  public changeActiveLink(element: any): void {
+    if(element === this.elemHome.nativeElement.text) {
+      this.elemHome.nativeElement.className = "nav-item nav-link active";
+    }
+    else {
+      this.elemHome.nativeElement.className = "nav-item nav-link";
+    }
+
+    if(element === this.elemProducts.nativeElement.text) {
+      this.elemProducts.nativeElement.className = "nav-item nav-link active";
+    }
+    else {
+      this.elemProducts.nativeElement.className = "nav-item nav-link";
+    }
+
+    if(element === this.elemServices.nativeElement.text) {
+      this.elemServices.nativeElement.className = "nav-item nav-link active";
+    }
+    else {
+      this.elemServices.nativeElement.className = "nav-item nav-link";
+    }
+
+    if(element === this.elemContactUs.nativeElement.text) {
+      this.elemContactUs.nativeElement.className = "nav-item nav-link active";
+    }
+    else {
+      this.elemContactUs.nativeElement.className = "nav-item nav-link";
+    }
+
+  }
 }
